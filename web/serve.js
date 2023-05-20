1194356b78028db085831575388ffdaecaddcf89
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-05-20 19:12:48 -0400

    a

---
+++ web/serve.js
@@ -0,0 +1,13 @@
+const exp = require('express');
+const app = exp();
+
+app.use(exp.static(__dirname + '/web'))
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html');
+
+})
+
+let port = 8080; 
+app.listen(port, (req, res) =>{
+    console.log(`Server listening on port #${port}`);
+}); 
